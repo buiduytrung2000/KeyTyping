3af384edc781ed1f9eaff69365aa6d674d39c2e4
bdt-lt <trungbdhe141714@fpt.edu.vn>
2021-10-19 17:45:56 +0700

    Add DAL

---
+++ DAL/BaseDAO.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public abstract class BaseDAO<T> {
+    protected Connection connection;
+    public BaseDAO()
+    {
+        try {
+            String user = "sa";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=ElectronicShop";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("connect success");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
